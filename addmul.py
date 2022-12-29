31f3243d0f402611d160032a4d4d3ffd25941205
ggulzarahmed <ggulzarahmed786@gmail.com>
2022-12-29 19:23:54 +0530

    trail2

---
+++ addmul.py
@@ -0,0 +1,8 @@
+num1=int(input("enter 1st number"))
+num2=int(input("enter the second number to be operate"))
+product=num1*num2
+if product<1000:
+    print("product is : ",product)
+
+else:
+    print("sum is : ",num1+num2)
